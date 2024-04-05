1cefbb568623d3685dbc5f866354bfde26fe7db1
AhmedJameel <roarme247@gmail.com>
2024-04-05 10:57:37 +0200

    up

--- orders/migrations/0001_initial.py
+++ orders/migrations/0001_initial.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('store', '0002_variation'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
