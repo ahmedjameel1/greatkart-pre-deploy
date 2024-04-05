1e59f5668d4c314705d08209937f2502fac6ee5e
AhmedJameel <roarme247@gmail.com>
2024-04-05 10:53:23 +0200

    update

--- orders/migrations/0003_auto_20210313_0218.py
+++ orders/migrations/0003_auto_20210313_0218.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0002_variation'),
         ('orders', '0002_auto_20210313_0211'),
     ]
 
